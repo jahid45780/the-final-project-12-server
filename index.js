2f2338a982f2b25eb72bb665bfd00a56daf6a039
jahid45780 <jahidhossain88899@gmail.com>
2023-12-01 03:37:59 +0600

    add the file

--- index.js
+++ index.js
@@ -29,12 +29,48 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+
+ const allArticleCollection = client.db('newsDB').collection('allArticles')
+ const trNewsCollection = client.db('newsDB').collection('trNews')
+  
+  
+ app.get('/allArticles', async (req, res)=>{
+
+          const limit = parseInt(req.query?.limit) || 0;
+          const result = await allArticleCollection.find().limit(limit).toArray()
+          res.send(result)
+           
+       })
+
+      //teNews
+
+      app.get('/trNews', async (req, res)=>{
+
+        const result = await  trNewsCollection.find().toArray()
+        res.send(result)
+         
+     })
+
+ // all service detail
+ app.get('/allNews/:id', async (req, res)=>{
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id)}
+  //  const options = { 
+  //  // Include only the `title` and `img` fields in each returned document
+  //   projection: {  title: 1, _id: 1, Image: 1, service_name: 1, service_description: 1, provider_img: 1, provider_name: 1, Service_Area: 1  },
+  //     };
+
+  const result = await allArticleCollection.findOne(query)
+  res.send(result)
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
